c977f50840c211ed9fb147fb3d769b05f6f62f77
Nerevin <kazak.redalerd@gmail.com>
2019-10-08 00:36:45 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let input =  expression.split('*');
+  let countTwo = 0;
+  let countFive = 0;
+
+  input.forEach(function (index) {
+    if (index.match(/.*!!/)) {
+      let a = parseInt(index);
+      let i = 0;
+      if (a % 2 == 0) {
+        i = 2;
+      }
+      else {
+        i = 3;
+      }
+      while (i <= a) {
+        for (let j = i; ; ) {
+          if (j % 5 == 0) {
+          	j /= 5
+            countFive++;
+          }
+          else if (j % 2 == 0) {
+          	j /= 2
+            countTwo++;
+          }
+          else break;
+        }
+        i += 2;
+      }
+    }
+    else {
+      let a = parseInt(index);
+      let i = 1;
+      while (i <= a) {
+        for (let j = i; ;) {
+          if (j % 5 == 0) {
+          	j /= 5
+            countFive++;
+          }
+          else if (j % 2 == 0) {
+          	j /= 2
+            countTwo++;
+          }
+          else break;
+        }
+        i++;
+      }
+    }
+  })
+  return Math.min(countTwo, countFive);
 }
